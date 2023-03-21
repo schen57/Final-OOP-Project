7834616fde74e40e74ad39187f07919e25dc7d47
Shengyu Chen <shengyuchen57@gmail.com>
2023-03-20 22:16:55 -0400

    New Scene, Player, Player Control
    1. Added a new scene where the game takes place
    2. Created a player sphere
    3. Implemented Player control where the sphere movement can be controlled

---
+++ OOP Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+
+    }
+}
